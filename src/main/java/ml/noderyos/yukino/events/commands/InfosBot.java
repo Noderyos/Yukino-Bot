a912e01d00f05786a23e16d9a12702e5aef51892
Noderyos <56077132+Noderyos@users.noreply.github.com>
2021-12-08 12:05:51 +0100

    Add files via upload

---
+++ src/main/java/ml/noderyos/yukino/events/commands/InfosBot.java
@@ -0,0 +1,13 @@
+package ml.noderyos.yukino.events.commands;
+public class InfosBot {
+    // Discord
+    public static final String PREFIX = ">";
+    public static final String TOKEN = "";
+
+    // Twitter
+    public static final String TwitterOAuthConsumerKey = "";
+    public static final String TwitterOAuthConsumerSecret = "";
+    public static final String TwitterOAuthAccessToken = "";
+    public static final String TwitterOAuthAccessTokenSecret = "";
+
+}
